8c7fbb15dda0384854161284d90dc5f7b4bff7f1
miickleyy <miickeyydlaz@gmail.com>
2025-03-13 19:16:09 -0600

    Add files via upload

---
+++ commands/help.js
@@ -0,0 +1,8 @@
+module.exports = (ctx) => {
+  ctx.reply(
+    'Comandos disponibles:\n' +
+    '/start - Inicia el bot\n' +
+    '/help - Muestra este mensaje de ayuda\n' +
+    '/hola - Saluda al bot'
+  );
+};
---
+++ commands/start.js
@@ -0,0 +1,3 @@
+module.exports = (ctx) => {
+  ctx.reply('¡Bienvenido a Strix-AI! Usa /help para ver los comandos disponibles.');
+};
