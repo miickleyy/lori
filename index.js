ffb878709105bd9be809f309dfd947a710093d4a
DexterZ9 <dexterdp909@gmail.com>
2025-03-13 19:53:16 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+const { Telegraf } = require('telegraf');
+const config = require('./config');
+
+const bot = new Telegraf(config.BOT_TOKEN);
+
+// Importa los comandos
+const startCommand = require('./commands/start');
+const helpCommand = require('./commands/help');
+const telmexCommand = require('./commands/telmex');
+
+// Configura los comandos
+bot.start((ctx) => startCommand(ctx));
+bot.help((ctx) => helpCommand(ctx));
+bot.command('telmex', (ctx) => telmexCommand(ctx));
+
+// Lanza el bot
+bot.launch()
+  .then(() => console.log('TelmexioriBot está en línea!'))
+  .catch((err) => console.error('Error al lanzar el bot:', err));
+
+// Manejo de apagado del bot de forma segura
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
